112d4fb91711e147c8600ca9a0ed7ab8304225a3
PranavGithubUser <pranavkavade9@gmail.com>
2025-12-08 19:24:14 +0530

    script.js updated

--- static/script.js
+++ static/script.js
@@ -80,9 +80,7 @@ function fetchRecipes() {
 
                                 <div class="flex items-center justify-between mt-4 pt-2 border-t border-gray-100 dark:border-gray-700">
                                     <span class="flex items-center gap-1 text-sm font-semibold text-gray-700 dark:text-gray-300">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-3 text-orange-500">
-                                            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clip-rule="evenodd" />
-                                        </svg>
+                
                                         ${recipe.prep_time}
                                     </span>
                                     
